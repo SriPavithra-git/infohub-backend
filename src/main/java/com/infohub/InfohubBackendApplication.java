f88230e33c9ecc0f32c2a0444edcb897b977a7ad
sri pavithra <sripavithrapayyavula2005@gmail.com>
2025-11-20 18:39:23 +0530

    Initial backend setup

---
+++ src/main/java/com/infohub/InfohubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.infohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfohubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfohubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infohub/InfohubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfohubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
